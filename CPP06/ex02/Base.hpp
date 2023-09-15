65b682c5c77d86017695c62a84bea4956918daa6
edern <edern56300@gmail.com>
2023-09-15 11:43:27 +0200

    ex 02 fini cpp 06 fini

--- CPP06/ex02/Base.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CPP06_BASE_HPP
-#define CPP06_BASE_HPP
-
-class Base
-{
-	virtual ~Base();
-};
-
-#endif //CPP06_BASE_HPP
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -14,27 +14,57 @@
 #include <time.h>
 #include <cstdlib>
 
-#include "Base.hpp"
-#include "A.hpp"
-#include "B.hpp"
-#include "C.hpp"
+class Base {
+public:
+	inline virtual ~Base() {};
+};
 
-Base * generate(void) {
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base *generate(void) {
+	Base *value;
 	srand(time(0));
 	int i = rand() % 3;
 	if (i == 0)
-		return static_cast
+		value = new A;
+	else if (i == 1)
+		value = new B;
+	else
+		value = new C;
+	std::cout << i << std::endl;
+	return value;
 }
 
 void identify(Base* p) {
-
+	if (dynamic_cast<A *>(p))
+		std::cout << "it's a A pointer" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "it's a B pointer" << std::endl;
+	else
+		std::cout << "it's a C pointer" << std::endl;
 }
 
 void identify(Base& p) {
-
+	try {
+		dynamic_cast<A &>(p);
+		std::cout << "it's a A reference" << std::endl;
+		return;
+	}
+	catch (...) {}
+	try {
+		dynamic_cast<B &>(p);
+		std::cout << "it's a B reference" << std::endl;
+		return;
+	}
+	catch (...) {}
+	std::cout << "it's a C reference" << std::endl;
 }
 
 int main() {
-
+	Base *value = generate();
+	identify(value);
+	identify(*value);
 	return 0;
 }
